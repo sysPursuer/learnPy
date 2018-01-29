f9e02da252cf5125bc577d23027b7420ae8f46b0
Pursuer <2568875447@qq.com>
2018-01-29 12:12:14 +0800

    add 参数‘

--- def_function.py
+++ def_function.py
@@ -19,3 +19,31 @@ x, y = move(100,100,60,math.pi / 6)
 print(x,y)
 t = move(100,100,60,math.pi / 6)
 print(t)
+
+def quardratic(a,b,c,x):
+	return a*x*x+b*x+c
+	
+print(quardratic(1,2,3,4))
+
+#可变参数	两种实现方式，两种调用方式
+def calc(numbers):
+	sum = 0
+	for n in numbers:
+		sum += n*n
+	return sum
+def calcu(*numbers):
+	sum = 0
+	for n in numbers:
+		sum += n*n
+	return sum
+print(calc([1,3]))
+nums = [1,2,3]
+print(calcu(1,2,3))
+print(calcu(*nums))
+#*nums表示把nums这个list的所有元素作为可变参数传进去。这种写法相当有用，而且很常见。
+
+def person(name,age,**kw):
+	print("name:",name,"age:",age,"other:",kw)
+
+extra = {'city:':'Beijing','job':'Engineer'}
+person('Tom',22,**extra)
